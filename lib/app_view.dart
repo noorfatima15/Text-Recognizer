d936047039fc7c75e051e1f491396c583443a669
noorfatima15 <noorfatima13689@gmail.com>
2023-04-12 12:28:19 +0500

    initial commit

---
+++ lib/app_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:text_scanner/home_screen.dart';
+
+class AppView extends StatelessWidget {
+  const AppView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:text_scanner/app_view.dart';
+
+void main() {
+  runApp(const AppView());
+}
